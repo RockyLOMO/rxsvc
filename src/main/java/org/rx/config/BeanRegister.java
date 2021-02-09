173b48d0737a0aa1b6178bb9aa2330a2153caad9
RockyLOMO <fake@rx.org>
2021-02-09 22:47:33 +0800

    test

--- src/main/java/org/rx/config/BeanRegister.java
+++ src/main/java/org/rx/config/BeanRegister.java
@@ -7,6 +7,7 @@ import org.rx.core.Strings;
 import org.rx.core.exception.InvalidException;
 import org.rx.redis.HybridCache;
 import org.rx.redis.RedisCache;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +17,7 @@ import static org.rx.core.Cache.LRU_CACHE;
 @Slf4j
 public class BeanRegister {
     @Bean
+    @ConditionalOnProperty(name = "app.redisUrl")
     public <TK, TV> RedisCache<TK, TV> redisCache(RedisConfig redisConfig) {
         if (Strings.isNullOrEmpty(redisConfig.getRedisUrl())) {
             throw new InvalidException("app.redisUrl is null");
