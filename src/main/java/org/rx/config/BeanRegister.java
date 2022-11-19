f6fcb65d16a74e9f7550efa844376a6efcdec00c
youfan <115806199+youfanx@users.noreply.github.com>
2022-11-19 11:56:38 +0800

    Update BeanRegister.java

--- src/main/java/org/rx/config/BeanRegister.java
+++ src/main/java/org/rx/config/BeanRegister.java
@@ -2,7 +2,7 @@ package org.rx.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.rx.core.Cache;
-import org.rx.core.Container;
+import org.rx.core.IOC;
 import org.rx.core.Strings;
 import org.rx.exception.InvalidException;
 import org.rx.redis.RedisCache;
@@ -10,8 +10,6 @@ import org.rx.redis.RedisCache;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import static org.rx.core.Cache.*;
 
 @Configuration
 @Slf4j
@@ -27,7 +25,7 @@ public class BeanRegister {
 
 //        RedisLocalCache<TK, TV> cache = new RedisLocalCache<>(redisConfig.getRedisUrl(), Cache.getInstance(MEMORY_CACHE));
         RedisCache<TK, TV> cache = new RedisCache<>(redisConfig.getRedisUrl());
-        Container.register(Cache.class, cache);
+        IOC.register(Cache.class, cache);
         log.info("register RedisCache ok");
         return cache;
     }
